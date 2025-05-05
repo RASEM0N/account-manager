ffabbbb269cc49178bd879c3bdd59afc3aadcbf8
Roman <@gmail.com>
2025-05-06 01:47:21 +0500

    feat: made useAccountValidation

--- src/entites/account/model/useAccountValidation.ts
+++ src/entites/account/model/useAccountValidation.ts
@@ -1,1 +1,31 @@
-export const useAccountValidation = () => {};
+import { RuleBuilder, useValidator } from '@/shared/lib/validator';
+import type { Account } from '@/entites/account';
+import { AccountType } from '@/entites/account';
+
+export const useAccountValidation = (account: Account) => {
+	return useValidator(
+		account,
+		new RuleBuilder<Account>()
+
+			.field('labels')
+			.string()
+			.max(50)
+			.next()
+
+			.field('login')
+			.string()
+			.required()
+			.max(100)
+			.next()
+
+			.field('password')
+			.when((_, ctx) => ctx.type === AccountType.local)
+			.string()
+			.required()
+			.max(1)
+			.max(100)
+			.next()
+
+			.done(),
+	);
+};
