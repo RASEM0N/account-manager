3e6aeae1a7305bf81d1e5d9946f60505bea49a85
Roman <@gmail.com>
2025-05-06 01:47:24 +0500

    fix: micro fix

--- src/features/accountsForm/ui/AccountsForm.vue
+++ src/features/accountsForm/ui/AccountsForm.vue
@@ -8,14 +8,14 @@ import { TEXT } from '@/shared/config/text';
 const store = useAccountStore();
 </script>
 <template>
-	<section class="space-y-4">
-		<div class="flex align-center">
+	<section>
+		<div class="flex align-center mb-10">
 			<h1 class="text-3xl font-bold mr-2">{{ TEXT.title }}</h1>
 			<Button variant="outline" size="icon" @click="store.add()">
 				<Plus />
 			</Button>
 		</div>
-    <div class="grid grid-cols-[repeat(11,1fr)] gap-3 items-start">
+    <div class="grid grid-cols-[repeat(11,1fr)] gap-3 items-start mb-4">
       <div class="col-span-3">{{ TEXT.rows.labels }}</div>
       <div class="col-span-2">{{ TEXT.rows.type }}</div>
       <div class="col-span-3">{{ TEXT.rows.login }}</div>
