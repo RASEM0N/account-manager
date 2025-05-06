6fcf3a7dae0022f2a48d81800e63f8b81ab957eb
Roman <@gmail.com>
2025-05-06 08:40:52 +0500

    chore: prettier

--- src/features/accountsForm/ui/AccountFormRow.vue
+++ src/features/accountsForm/ui/AccountFormRow.vue
@@ -30,7 +30,7 @@ const isShowPassword = computed(() => values.type === AccountType.local);
 			v-model="values.labels"
 			:placeholder="TEXT.placeholders.labels"
 			:error="!!errors.labels"
-      maxlength="50"
+			maxlength="50"
 			@blur="onBlur"
 		/>
 
@@ -61,7 +61,7 @@ const isShowPassword = computed(() => values.type === AccountType.local);
 			v-model="values.login"
 			:placeholder="TEXT.placeholders.login"
 			:error="!!errors.login"
-      maxlength="100"
+			maxlength="100"
 			@blur="onBlur"
 		/>
 
@@ -72,7 +72,7 @@ const isShowPassword = computed(() => values.type === AccountType.local);
 			v-model="values.password"
 			:placeholder="TEXT.placeholders.password"
 			:error="!!errors.password"
-      maxlength="100"
+			maxlength="100"
 			@blur="onBlur"
 		/>
 
